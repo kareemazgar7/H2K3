a51c694a6199f28e6fac9582e3a808a995067f72
kareemazgar7 <kareemazgar07@gmail.com>
2021-05-20 20:06:59 -0400

    new class created

---
+++ src/practice/PullandPushandMerge.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class PullandPushandMerge {
+
+	public static void main(String[] args) {
+		
+		int number  = 7;
+		 String name = "ali";
+		 
+		 System.out.println(name);
+		
+
+	}
+
+}
